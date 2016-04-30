fa9fadfb28071ed57a089c312bcfb6a26121b744
roselifernandesgaldino <roselifernandesgaldino@gmail.com>
2016-04-30 18:18:36 -0300

    iniciando o projeto

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('contatoCtrl', function($scope) {
+
+})
+   
+.controller('produtoCtrl', function($scope) {
+
+})
+    
